1da644daf1e7ec7758ee3633a8c6f8ac6d554ac3
menna gamal <95531094+Mennagamal272@users.noreply.github.com>
2023-03-08 05:29:45 +0200

    Update problem8.c

--- problem8.c
+++ problem8.c
@@ -3,6 +3,10 @@ int main(){
    int n,factorial=1 ;
    printf("enter number : ");
    scanf("%d",&n);
+   if(n < 0){
+    printf("invalid input");
+    return 0;
+   }
    for(int i =n;i>=1;i--){
     factorial*=i;
    }
